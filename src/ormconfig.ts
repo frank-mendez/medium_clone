682cfc3c3f8193bb5a2d68c0b1ad369967a402ab
frankmendez <f.mendez@arcanys.com>
2021-11-08 21:28:13 +0800

    Configuring database

---
+++ src/ormconfig.ts
@@ -0,0 +1,13 @@
+import { ConnectionOptions } from "typeorm"
+
+
+const config: ConnectionOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'mediumclone',
+    password: '123',
+    database: 'mediumclone'
+}
+
+export default config
