3ce575dc2c58324576f0c5e67bd9ceb9262dd2b3
frankmendez <frankmendezresources@gmail.com>
2021-11-09 17:44:00 +0800

    Working with tags repository

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TagEntity } from './tag.entity';
 
 @Injectable()
 export class TagService {
-  findAll(): string[] {
-    return ['dragon', 'coffee', 'soju', 'beer'];
+  constructor(@InjectRepository(TagEntity) private readonly tagRepository: Repository<TagEntity>){}
+
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find()
   }
 }
