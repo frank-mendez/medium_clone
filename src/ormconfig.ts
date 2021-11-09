54dc77c7c555c4f519749b96e3ec96d1c519192e
frankmendez <frankmendezresources@gmail.com>
2021-11-09 17:19:00 +0800

    Creating tag entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,7 +7,9 @@ const config: ConnectionOptions = {
     port: 5432,
     username: 'mediumclone',
     password: '123',
-    database: 'mediumclone'
+    database: 'mediumclone',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true
 }
 
 export default config
