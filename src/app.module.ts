ebac23808e6bd2ba50138df88631ef03f85a7c5c
frankmendez <frankmendezresources@gmail.com>
2021-11-10 17:11:51 +0800

    preparing register request

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from '@app/ormconfig';
+import { UserModule } from '@app/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config),TagModule],
+  imports: [TypeOrmModule.forRoot(config),TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
